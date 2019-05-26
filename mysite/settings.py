a635569688e33561b3fc45f1abbcc89494c9abe6
tayqinyi <37528962+tayqinyi@users.noreply.github.com>
2019-05-26 20:00:54 +0800

    Furhter update settings file

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/var/www/mysite/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/var/www/mysite/media/'
 
 TINYMCE_DEFAULT_CONFIG = {
     'height': 360,
